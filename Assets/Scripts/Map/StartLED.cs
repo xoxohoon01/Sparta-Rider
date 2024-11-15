46456aa4e52dbbb48289cb1154505d68e38387a1
tive1021 <tive1021@naver.com>
2024-11-15 20:18:10 +0900

    에셋 추가

---
+++ Assets/Scripts/Map/StartLED.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLED : MonoBehaviour
+{
+    [SerializeField] Material StartLEDRed;
+    [SerializeField] Material StartLEDGreen;
+
+    [SerializeField] GameObject lightA;
+    [SerializeField] GameObject lightB;
+    [SerializeField] GameObject lightC;
+    
+}

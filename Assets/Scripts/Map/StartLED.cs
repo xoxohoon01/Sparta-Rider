fea537571fe18b1262178e6c0460b2426e09e71b
tive1021 <tive1021@naver.com>
2024-11-15 20:38:01 +0900

    led 설정

--- Assets/Scripts/Map/StartLED.cs
+++ Assets/Scripts/Map/StartLED.cs
@@ -7,8 +7,27 @@ public class StartLED : MonoBehaviour
     [SerializeField] Material StartLEDRed;
     [SerializeField] Material StartLEDGreen;
 
-    [SerializeField] GameObject lightA;
-    [SerializeField] GameObject lightB;
-    [SerializeField] GameObject lightC;
+    [SerializeField] MeshRenderer lightA;
+    [SerializeField] MeshRenderer lightB;
+    [SerializeField] MeshRenderer lightC;
+
+    WaitForSeconds wait = new WaitForSeconds(1);
     
+    void Start()
+    {
+        StartCoroutine(ChangeLights());
+    }
+
+    IEnumerator ChangeLights()
+    {
+        lightA.material = StartLEDRed;
+        yield return wait;
+        lightB.material = StartLEDRed;
+        yield return wait;
+        lightC.material = StartLEDRed;
+        yield return wait;
+        lightA.material = StartLEDGreen; 
+        lightB.material = StartLEDGreen;
+        lightC.material = StartLEDGreen;
+    }
 }

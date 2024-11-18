12f2362774799722999f535e75b005d08b32cf11
seo600 <conrad270@naver.com>
2024-11-18 14:02:49 +0900

    Update GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public enum SceneType
 {
     StartScene,
-    MapScene,
+    MapChoiceScene,
     CarScene,
     LevelScene,
     MainScene
@@ -18,4 +18,11 @@ public class GameManager : MonoSingleton<GameManager>
     {
         SceneManager.LoadScene($"{sceneType.ToString()}");
     }
+
+    // Button 전용 메서드
+    public void LoadStartScene() => LoadScene(SceneType.StartScene);
+    public void LoadMapChoiceScene() => LoadScene(SceneType.MapChoiceScene);
+    public void LoadCarScene() => LoadScene(SceneType.CarScene);
+    public void LoadLevelScene() => LoadScene(SceneType.LevelScene);
+    public void LoadMainScene() => LoadScene(SceneType.MainScene);
 }

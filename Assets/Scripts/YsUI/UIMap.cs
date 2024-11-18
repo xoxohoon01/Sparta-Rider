2307aabffc8020a74c82a2f1a2e1aa8b9758773a
seo600 <conrad270@naver.com>
2024-11-18 20:12:20 +0900

    Complex: UIMap & MapTrack
    Update MapTrack

---
+++ Assets/Scripts/YsUI/UIMap.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using DG.Tweening;
+
+public class UIMap : MonoBehaviour
+{
+    [SerializeField] private GameObject mapObject;
+
+    public void OnMap()
+    {
+        
+        //mapObject.transform.DOLocalMoveX
+    }
+}

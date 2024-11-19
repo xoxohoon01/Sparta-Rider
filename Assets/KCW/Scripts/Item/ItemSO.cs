be8c429f6be315fe19ce146e97c5a533546d093c
simple2126 <kimcw212@gmail.com>
2024-11-19 22:25:58 +0900

    feat: item effect 구현 완료

--- Assets/KCW/Scripts/Item/ItemSO.cs
+++ Assets/KCW/Scripts/Item/ItemSO.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ItemSO : ScriptableObject
 {
     public ItemName itemName;
+    public ItemType itemType;
     public float durationTime; // 아이템 효과 지속시간
     public float speed;
 }

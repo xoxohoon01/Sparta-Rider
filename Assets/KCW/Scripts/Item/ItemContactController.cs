f3b6cb789173f9f41bbfd3c36527795003175698
simple2126 <kimcw212@gmail.com>
2024-11-20 22:42:24 +0900

    fix: itemMovement 분리
    ItemMovement와 ItemContactController로 분리

---
+++ Assets/KCW/Scripts/Item/ItemContactController.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Properties;
+using UnityEngine;
+
+public class ItemContactController : MonoBehaviour
+{
+    [SerializeField] private ItemSO itemSO;
+    private Renderer ren;
+    private Collider col;
+
+    private VehicleController vehicleController;
+
+    private void Awake()
+    {
+        ren = GetComponent<Renderer>();
+        col = GetComponent<Collider>();
+    }
+
+    public void SetVehicleController(VehicleController vController)
+    {
+        vehicleController = vController;
+        vehicleController.totalRotate = 0f;
+        vehicleController.itemSO = itemSO;
+    }
+
+    private void disableItem()
+    {
+        ren.enabled = false;
+        col.enabled = false;
+    }
+
+    private void enableItem()
+    {
+        ren.enabled = true;
+        ren.enabled = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player") ||
+            collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            disableItem();
+            SetVehicleController(collision.gameObject.GetComponent<VehicleController>());
+            switch (itemSO.itemName)
+            {
+                case ItemName.Banana: CollideBanana(vehicleController.itemAccelerationMultiplier); break;
+                case ItemName.Tomato: CollideTomato(); break;
+                case ItemName.Coffee: CollideCoffee(vehicleController.itemAccelerationMultiplier); break;
+                case ItemName.Cake: CollideCake(vehicleController.itemAccelerationMultiplier); break;
+                case ItemName.Watermelon: CollideWatermelon(vehicleController.itemAccelerationMultiplier); break;
+            }
+        }
+    }
+
+    // 바나나 밟을 때
+    private void CollideBanana(float initialSpeed)
+    {
+        vehicleController.isBanana = true;
+        vehicleController.itemAccelerationMultiplier = 0f;
+        StartCoroutine(CoCollideBanana(initialSpeed));
+    }
+
+    private IEnumerator CoCollideBanana(float initialSpeed)
+    {
+        yield return new WaitForSeconds(itemSO.durationTime);
+        vehicleController.isBanana = false;
+        vehicleController.itemAccelerationMultiplier = initialSpeed;
+        enableItem();
+        gameObject.SetActive(false);
+    }
+
+    // 토마토 맞으면 화면 붉은색으로 변경 
+    private void CollideTomato()
+    {
+        GameObject _effect = ItemManager.Instance.tomatoEffect;
+        _effect.SetActive(true);
+        StartCoroutine(CoCollideTomato(_effect));
+    }
+
+    private IEnumerator CoCollideTomato(GameObject image)
+    {
+        yield return new WaitForSeconds(itemSO.durationTime);
+        enableItem();
+        image.gameObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    // 커피 사용하면 속도 2배
+    private void CollideCoffee(float initialSpeed)
+    {
+        vehicleController.itemAccelerationMultiplier *= 2f;
+        StartCoroutine(CoCollideCoffee(initialSpeed));
+    }
+
+    private IEnumerator CoCollideCoffee(float initialSpeed)
+    {
+        yield return new WaitForSeconds(itemSO.durationTime);
+        vehicleController.itemAccelerationMultiplier = initialSpeed;
+    }
+
+    private void CollideCake(float initialSpeed)
+    {
+        vehicleController.itemAccelerationMultiplier *= 0.5f;
+        StartCoroutine(CoCollideCake(initialSpeed));
+    }
+
+    private IEnumerator CoCollideCake(float initialSpeed)
+    {
+        yield return new WaitForSeconds(itemSO.durationTime);
+        vehicleController.itemAccelerationMultiplier = initialSpeed;
+        enableItem();
+        gameObject.SetActive(false);
+    }
+
+    // 수박 맞으면 정해진 시간동안 멈춤
+    private void CollideWatermelon(float initialSpeed)
+    {
+        vehicleController.itemAccelerationMultiplier = 0f;
+    }
+
+    private IEnumerator CoCollideWatermelon(float initialSpeed)
+    {
+        yield return new WaitForSeconds(itemSO.durationTime);
+        vehicleController.itemAccelerationMultiplier = initialSpeed;
+        enableItem();
+        gameObject.SetActive(false);
+    }
+}
--- Assets/KCW/Scripts/Item/ItemMovement.cs
+++ Assets/KCW/Scripts/Item/ItemMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public class ItemMovement : MonoBehaviour
 {
     [SerializeField] private ItemSO itemSO;
+    private ItemContactController contactController;
     private Vector3 moveDirection = Vector3.zero;
     private Rigidbody rb;
-    private Renderer ren;
     private Collider col;
     private float itemSizeZ;
     private float itemSizeY;
@@ -15,8 +15,8 @@ public class ItemMovement : MonoBehaviour
 
     private void Awake()
     {
+        contactController = GetComponent<ItemContactController>();
         rb = GetComponent<Rigidbody>();
-        ren = GetComponent<Renderer>();
         col = GetComponent<Collider>();
         itemSizeZ = col.bounds.size.z;
         itemSizeY = col.bounds.size.y;
@@ -30,17 +30,9 @@ public class ItemMovement : MonoBehaviour
         }
     }
 
-    public void SetVehicleController(VehicleController vController)
-    {
-        vehicleController = vController;
-        vehicleController.totalRotate = 0f;
-        vehicleController.itemSO = itemSO;
-    }
-
     // 움직이는 객체만 Move로 이동
     public void CheckMoveItem(Vector3 forward, VehicleController vController)
     {
-        SetVehicleController(vController);
         SetPosition();
         if (itemSO.itemType == ItemType.Move) Move(forward);
     }
@@ -57,114 +49,5 @@ public class ItemMovement : MonoBehaviour
         if (itemSO.itemType == ItemType.Move) transform.position += itemSizeZ * 1f * transform.forward;
         else transform.position += itemSizeZ * 0.5f * -transform.forward;
         transform.position += itemSizeY * 0.5f * Vector3.up + new Vector3(0, 0.1f, 0);
-    }
-
-    private void disableItem()
-    {
-        ren.enabled = false;
-        col.enabled = false;
-    }
-
-    private void enableItem()
-    {
-        ren.enabled = true;
-        ren.enabled = true;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
-        {
-            gameObject.SetActive(false);
-        }
-
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Player") || 
-            collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
-        {
-            disableItem();
-
-            switch (itemSO.itemName)
-            {
-                case ItemName.Banana: CollideBanana(vehicleController.itemAccelerationMultiplier); break;
-                case ItemName.Tomato: CollideTomato(); break;
-                case ItemName.Coffee: CollideCoffee(vehicleController.itemAccelerationMultiplier); break;
-                case ItemName.Cake: CollideCake(vehicleController.itemAccelerationMultiplier); break;
-                case ItemName.Watermelon: CollideWatermelon(vehicleController.itemAccelerationMultiplier); break;
-            }
-        }
-    }
-
-    // 바나나 밟을 때
-    private void CollideBanana(float initialSpeed)
-    {
-        vehicleController.isBanana = true;
-        vehicleController.itemAccelerationMultiplier = 0f;
-        StartCoroutine(CoCollideBanana(initialSpeed));
-    }
-
-    private IEnumerator CoCollideBanana(float initialSpeed)
-    {
-        yield return new WaitForSeconds(itemSO.durationTime);
-        vehicleController.isBanana = false;
-        vehicleController.itemAccelerationMultiplier = initialSpeed;
-        enableItem();
-        gameObject.SetActive(false);
-    }
-
-    // 토마토 맞으면 화면 붉은색으로 변경 
-    private void CollideTomato()
-    {
-        GameObject _effect = ItemManager.Instance.tomatoEffect;
-        _effect.SetActive(true);
-        StartCoroutine(CoCollideTomato(_effect));
-    }
-
-    private IEnumerator CoCollideTomato(GameObject image)
-    {
-        yield return new WaitForSeconds(itemSO.durationTime);
-        enableItem();
-        image.gameObject.SetActive(false);
-        gameObject.SetActive(false);
-    }
-
-    // 커피 사용하면 속도 2배
-    private void CollideCoffee(float initialSpeed)
-    {
-        vehicleController.itemAccelerationMultiplier *= 2f;
-        StartCoroutine(CoCollideCoffee(initialSpeed));
-    }
-
-    private IEnumerator CoCollideCoffee(float initialSpeed)
-    {
-        yield return new WaitForSeconds(itemSO.durationTime);
-        vehicleController.itemAccelerationMultiplier = initialSpeed;
-    }
-
-    private void CollideCake(float initialSpeed)
-    {
-        vehicleController.itemAccelerationMultiplier *= 0.5f;
-        StartCoroutine(CoCollideCake(initialSpeed));
-    }
-
-    private IEnumerator CoCollideCake(float initialSpeed)
-    {
-        yield return new WaitForSeconds(itemSO.durationTime);
-        vehicleController.itemAccelerationMultiplier = initialSpeed;
-        enableItem();
-        gameObject.SetActive(false);
-    }
-
-    // 수박 맞으면 정해진 시간동안 멈춤
-    private void CollideWatermelon(float initialSpeed)
-    {
-        vehicleController.itemAccelerationMultiplier = 0f;
-    }
-
-    private IEnumerator CoCollideWatermelon(float initialSpeed)
-    {
-        yield return new WaitForSeconds(itemSO.durationTime);
-        vehicleController.itemAccelerationMultiplier = initialSpeed;
-        enableItem();
-        gameObject.SetActive(false);
     }
 }

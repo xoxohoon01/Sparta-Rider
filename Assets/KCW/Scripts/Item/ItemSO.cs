8574a80f0a4b8a472e7a7ee230c6ced68e9830cb
simple2126 <kimcw212@gmail.com>
2024-11-20 21:04:54 +0900

    fix: 바나나 회전 수정

--- Assets/KCW/Scripts/Item/ItemSO.cs
+++ Assets/KCW/Scripts/Item/ItemSO.cs
@@ -8,5 +8,6 @@ public class ItemSO : ScriptableObject
     public ItemName itemName;
     public ItemType itemType;
     public float durationTime; // 아이템 효과 지속시간
+    public float rotationNum;
     public float speed;
 }

541fd938a35868eddd02fbd979f5fc6c3c7014d9
tive1021 <tive1021@naver.com>
2024-11-21 22:12:36 +0900

    Update MidPointManager.cs

--- Assets/Scripts/Managers/MidPointManager.cs
+++ Assets/Scripts/Managers/MidPointManager.cs
@@ -21,6 +21,9 @@ public class MidPointManager : DestroySingleton<MidPointManager>
     {
         base.Awake();
 
+        // 멈춘 시간 초기화
+        Time.timeScale = 1.0f;
+
         // bestLapTime, isClear 초기화
         bestLapTime = float.MaxValue;
         isClear = false;
